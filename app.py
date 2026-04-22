c66a5a1969d88614e351b15f159bdf894b083c71
TitusCitrus <siscuswijaya@gmail.com>
2026-04-22 18:42:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,32 @@ import streamlit as st
 import pandas as pd
 from backend import run_dynamic_forecast 
 import plotly.graph_objects as go
+import streamlit.components.v1 as components
+
+def inject_mixpanel():
+    # This JS reaches into the parent window (the main Streamlit app) and injects Mixpanel
+    mixpanel_js = """
+    <script type="text/javascript">
+        if (window.parent !== window) {
+            var parentDocument = window.parent.document;
+            var script = parentDocument.createElement('script');
+            script.type = 'text/javascript';
+            script.innerHTML = `
+                (function(e,c){if(!c.__SV){var l,h;window.mixpanel=c;c._i=[];c.init=function(q,r,f){function t(d,a){var g=a.split(".");2==g.length&&(d=d[g[0]],a=g[1]);d[a]=function(){d.push([a].concat(Array.prototype.slice.call(arguments,0)))}}var b=c;"undefined"!==typeof f?b=c[f]=[]:f="mixpanel";b.people=b.people||[];b.toString=function(d){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);d||(a+=" (stub)");return a};b.people.toString=function(){return b.toString(1)+".people (stub)"};l="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders start_session_recording stop_session_recording people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
+                for(h=0;h<l.length;h++)t(b,l[h]);var n="set set_once union unset remove delete".split(" ");b.get_group=function(){function d(p){a[p]=function(){b.push([g,[p].concat(Array.prototype.slice.call(arguments,0))])}}for(var a={},g=["get_group"].concat(Array.prototype.slice.call(arguments,0)),m=0;m<n.length;m++)d(n[m]);return a};c._i.push([q,r,f])};c.__SV=1.2;var k=e.createElement("script");k.type="text/javascript";k.async=!0;k.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=e.getElementsByTagName("script")[0];e.parentNode.insertBefore(k,e)}})(document,window.mixpanel||[]);
+                
+                mixpanel.init('63a4f4f8d971e9838dc46e709a68bfb4', {
+                    autocapture: true,
+                    record_sessions_percent: 100,
+                    api_host: 'https://api-eu.mixpanel.com',
+                });
+            `;
+            parentDocument.head.appendChild(script);
+        }
+    </script>
+    """
+    # Render an invisible 0x0 component to execute the script
+    components.html(mixpanel_js, width=0, height=0)
 
 # ==========================================
 # 1. GAUGE CHART FUNCTION 
@@ -159,6 +185,7 @@ def draw_broker_inventory_chart(broker_file):
 # 4. STREAMLIT UI
 # ==========================================
 st.set_page_config(page_title="AI Trading Forecast", page_icon="📈", layout="wide")
+inject_mixpanel()
 
 st.title("Dynamic Algorithmic Price Prediction Model")
 
